7f154f817b3350a5cde851634bdcd8ede19b3db6
nhubinh <trinhthibiihnhu@gmail.com>
2025-11-26 22:17:45 +0700

    Update src for Gunny

--- scr/main/GamePanel.java
+++ scr/main/GamePanel.java
@@ -1,35 +1,43 @@
 package main;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JPanel;
 
-import static main.Game.*;
+import inputs.MouseInputs;
+import static main.Game.GAME_HEIGHT;
+import static main.Game.GAME_WIDTH;
 
 public class GamePanel extends JPanel {
+
+    private MouseInputs mouseInputs;
     private Game game;
 
     public GamePanel(Game game) {
+        mouseInputs = new MouseInputs(this);
         this.game = game;
-
         setPanelSize();
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
     private void setPanelSize() {
         Dimension size = new Dimension(GAME_WIDTH, GAME_HEIGHT);
         setPreferredSize(size);
-        System.out.println("size: " + GAME_WIDTH + " : " + GAME_HEIGHT);
+    }
+
+    public void updateGame() {
+
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        //game.render(g);
+        game.render(g);
     }
 
     public Game getGame() {
         return game;
     }
+
 }
