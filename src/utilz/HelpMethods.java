523f5ba811cc1de8cc5ce31140a2e61cb31cb595
ThDat4505 <datnguyen0571@gmail.com>
2025-11-27 13:51:14 +0700

    add more help method for magicbean, loadsave magic bean picture

--- src/utilz/HelpMethods.java
+++ src/utilz/HelpMethods.java
@@ -1,31 +1,28 @@
 package utilz;
 
-import static utilz.Constants.EnemyConstants.CRABBY;
-import static utilz.Constants.ObjectConstants.*;
+import main.Game;
+import entities.Crabby;
+import objects.*;
 
-import java.awt.Color;
-import java.awt.Point;
+import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
-import entities.Crabby;
-import main.Game;
-import objects.Cannon;
-import objects.GameContainer;
-import objects.Potion;
-import objects.Projectile;
-import objects.Spike;
+import static utilz.Constants.EnemyConstants.CRABBY;
+import static utilz.Constants.ObjectConstants.*;
 
 public class HelpMethods {
 
     public static boolean CanMoveHere(float x, float y, float width, float height, int[][] lvlData) {
+
         if (!IsSolid(x, y, lvlData))
-            if (!IsSolid(x + width, y + height, lvlData))
-                if (!IsSolid(x + width, y, lvlData))
-                    if (!IsSolid(x, y + height, lvlData))
+            if(!IsSolid(x + width, y + height, lvlData))
+                if(!IsSolid(x+width, y, lvlData))
+                    if(!IsSolid(x, y + height, lvlData))
                         return true;
         return false;
+
     }
 
     private static boolean IsSolid(float x, float y, int[][] lvlData) {
@@ -34,95 +31,103 @@ public class HelpMethods {
             return true;
         if (y < 0 || y >= Game.GAME_HEIGHT)
             return true;
+
         float xIndex = x / Game.TILES_SIZE;
         float yIndex = y / Game.TILES_SIZE;
 
         return IsTileSolid((int) xIndex, (int) yIndex, lvlData);
+
     }
 
     public static boolean IsProjectileHittingLevel(Projectile p, int[][] lvlData) {
-        return IsSolid(p.getHitbox().x + p.getHitbox().width / 2, p.getHitbox().y + p.getHitbox().height / 2, lvlData);
-
+        return IsSolid(p.getHitbox().x + p.getHitbox().width/2, p.getHitbox().y + p.getHitbox().height/2, lvlData);
+    }
+    public static boolean IsPlayerProjectileHittingLevel(PlayerProjectile pp, int[][] lvlData) {
+        return IsSolid(pp.getHitbox().x + pp.getHitbox().width/2, pp.getHitbox().y + pp.getHitbox().height/2, lvlData);
     }
 
     public static boolean IsTileSolid(int xTile, int yTile, int[][] lvlData) {
         int value = lvlData[yTile][xTile];
 
-        if (value >= 48 || value < 0 || value != 11)
+        if(value >= 48 || value < 0 || value != 11)
             return true;
         return false;
     }
 
     public static float GetEntityXPosNextToWall(Rectangle2D.Float hitbox, float xSpeed) {
-        int currentTile = (int) (hitbox.x / Game.TILES_SIZE);
-        if (xSpeed > 0) {
-            // Right
+        int currentTile = (int) hitbox.x/Game.TILES_SIZE;
+        if( xSpeed > 0) {
+            //right
             int tileXPos = currentTile * Game.TILES_SIZE;
             int xOffset = (int) (Game.TILES_SIZE - hitbox.width);
             return tileXPos + xOffset - 1;
-        } else
-            // Left
+        } else {
+            //left
             return currentTile * Game.TILES_SIZE;
+        }
     }
 
     public static float GetEntityYPosUnderRoofOrAboveFloor(Rectangle2D.Float hitbox, float airSpeed) {
-        int currentTile = (int) (hitbox.y / Game.TILES_SIZE);
-        if (airSpeed > 0) {
-            // Falling - touching floor
+        int currentTile = (int) hitbox.y/Game.TILES_SIZE;
+        if(airSpeed > 0) {
+            //Falling - touching floor
             int tileYPos = currentTile * Game.TILES_SIZE;
             int yOffset = (int) (Game.TILES_SIZE - hitbox.height);
             return tileYPos + yOffset - 1;
-        } else
-            // Jumping
+        } else {
+            //JUMPING
             return currentTile * Game.TILES_SIZE;
-
+        }
     }
 
     public static boolean IsEntityOnFloor(Rectangle2D.Float hitbox, int[][] lvlData) {
-        if (!IsSolid(hitbox.x, hitbox.y + hitbox.height + 1, lvlData))
-            if (!IsSolid(hitbox.x + hitbox.width, hitbox.y + hitbox.height + 1, lvlData))
+        //check the pixel below bottom left and right corner
+        if (!IsSolid(hitbox.x, hitbox.y + hitbox.height+1, lvlData)) {
+            if(!IsSolid(hitbox.x+hitbox.width, hitbox.y + hitbox.height+1, lvlData))
                 return false;
+        }
         return true;
     }
 
     public static boolean IsFloor(Rectangle2D.Float hitbox, float xSpeed, int[][] lvlData) {
-        if (xSpeed > 0)
-            return IsSolid(hitbox.x + hitbox.width + xSpeed, hitbox.y + hitbox.height + 1, lvlData);
-        else
+        if(xSpeed > 0) {
+            return IsSolid(hitbox.x + hitbox.width, hitbox.y + hitbox.height + 1, lvlData);
+        } else {
             return IsSolid(hitbox.x + xSpeed, hitbox.y + hitbox.height + 1, lvlData);
+        }
     }
 
-    public static boolean CanCannonSeePlayer(int[][] lvlData, Rectangle2D.Float firstHitbox, Rectangle2D.Float secondHitbox, int yTile) {
+    public static boolean CanCannonSeePlayer(int[][] lvlData,Rectangle2D.Float firstHitbox, Rectangle2D.Float secondHitbox, int yTile) {
         int firstXTile = (int) (firstHitbox.x / Game.TILES_SIZE);
         int secondXTile = (int) (secondHitbox.x / Game.TILES_SIZE);
 
-        if (firstXTile > secondXTile)
+        if(firstXTile > secondXTile)
             return IsAllTilesClear(secondXTile, firstXTile, yTile, lvlData);
         else
             return IsAllTilesClear(firstXTile, secondXTile, yTile, lvlData);
     }
 
     public static boolean IsAllTilesClear(int xStart, int xEnd, int y, int[][] lvlData) {
-        for (int i = 0; i < xEnd - xStart; i++)
+        for(int i = 0; i < (xEnd - xStart); i++)
             if (IsTileSolid(xStart + i, y, lvlData))
                 return false;
         return true;
     }
 
     public static boolean IsAllTilesWalkable(int xStart, int xEnd, int y, int[][] lvlData) {
-        if (IsAllTilesClear(xStart, xEnd, y, lvlData))
-            for (int i = 0; i < xEnd - xStart; i++) {
+        if(IsAllTilesClear(xStart, xEnd, y, lvlData))
+            for(int i = 0; i < (xEnd - xStart); i++) {
                 if (!IsTileSolid(xStart + i, y + 1, lvlData))
                     return false;
             }
         return true;
     }
 
-    public static boolean IsSightClear(int[][] lvlData, Rectangle2D.Float firstHitbox, Rectangle2D.Float secondHitbox, int yTile) {
+    public static boolean IsSightClear(int[][] lvlData,Rectangle2D.Float firstHitbox, Rectangle2D.Float secondHitbox, int yTile) {
         int firstXTile = (int) (firstHitbox.x / Game.TILES_SIZE);
         int secondXTile = (int) (secondHitbox.x / Game.TILES_SIZE);
 
-        if (firstXTile > secondXTile)
+        if(firstXTile > secondXTile)
             return IsAllTilesWalkable(secondXTile, firstXTile, yTile, lvlData);
         else
             return IsAllTilesWalkable(firstXTile, secondXTile, yTile, lvlData);
@@ -130,8 +135,8 @@ public class HelpMethods {
 
     public static int[][] GetLevelData(BufferedImage img) {
         int[][] lvlData = new int[img.getHeight()][img.getWidth()];
-        for (int j = 0; j < img.getHeight(); j++)
-            for (int i = 0; i < img.getWidth(); i++) {
+        for(int j = 0; j < img.getHeight(); j++)
+            for(int i = 0; i < img.getWidth(); i++) {
                 Color color = new Color(img.getRGB(i, j));
                 int value = color.getRed();
                 if (value >= 48)
@@ -141,10 +146,10 @@ public class HelpMethods {
         return lvlData;
     }
 
-    public static ArrayList<Crabby> GetCrabs(BufferedImage img) {
+    public static ArrayList<Crabby> Getcrabs(BufferedImage img) {
         ArrayList<Crabby> list = new ArrayList<>();
-        for (int j = 0; j < img.getHeight(); j++)
-            for (int i = 0; i < img.getWidth(); i++) {
+        for(int j = 0; j < img.getHeight(); j++)
+            for(int i = 0; i < img.getWidth(); i++) {
                 Color color = new Color(img.getRGB(i, j));
                 int value = color.getGreen();
                 if (value == CRABBY)
@@ -154,8 +159,8 @@ public class HelpMethods {
     }
 
     public static Point GetPlayerSpawn(BufferedImage img) {
-        for (int j = 0; j < img.getHeight(); j++)
-            for (int i = 0; i < img.getWidth(); i++) {
+        for(int j = 0; j < img.getHeight(); j++)
+            for(int i = 0; i < img.getWidth(); i++) {
                 Color color = new Color(img.getRGB(i, j));
                 int value = color.getGreen();
                 if (value == 100)
@@ -164,58 +169,87 @@ public class HelpMethods {
         return new Point(1 * Game.TILES_SIZE, 1 * Game.TILES_SIZE);
     }
 
+    public static Point GetMagicBeanSpawn(BufferedImage img) {
+        for(int j = 0; j < img.getHeight(); j++)
+            for(int i = 0; i < img.getWidth(); i++) {
+                Color color = new Color(img.getRGB(i, j));
+                int value = color.getGreen();
+                if (value == 120)
+                    return new Point(i * Game.TILES_SIZE, j * Game.TILES_SIZE);
+            }
+        return new Point(1 * Game.TILES_SIZE, 1 * Game.TILES_SIZE);
+    }
+
     public static ArrayList<Potion> GetPotions(BufferedImage img) {
         ArrayList<Potion> list = new ArrayList<>();
-        for (int j = 0; j < img.getHeight(); j++)
-            for (int i = 0; i < img.getWidth(); i++) {
+        for(int j = 0; j < img.getHeight(); j++)
+            for(int i = 0; i < img.getWidth(); i++) {
                 Color color = new Color(img.getRGB(i, j));
                 int value = color.getBlue();
                 if (value == RED_POTION || value == BLUE_POTION)
                     list.add(new Potion(i * Game.TILES_SIZE, j * Game.TILES_SIZE, value));
             }
-
         return list;
     }
 
     public static ArrayList<GameContainer> GetContainers(BufferedImage img) {
         ArrayList<GameContainer> list = new ArrayList<>();
-        for (int j = 0; j < img.getHeight(); j++)
-            for (int i = 0; i < img.getWidth(); i++) {
+        for(int j = 0; j < img.getHeight(); j++)
+            for(int i = 0; i < img.getWidth(); i++) {
                 Color color = new Color(img.getRGB(i, j));
                 int value = color.getBlue();
                 if (value == BOX || value == BARREL)
                     list.add(new GameContainer(i * Game.TILES_SIZE, j * Game.TILES_SIZE, value));
             }
-
         return list;
     }
 
-    public static ArrayList<Spike> GetSpikes(BufferedImage img) {
-        ArrayList<Spike> list = new ArrayList<>();
 
-        for (int j = 0; j < img.getHeight(); j++)
-            for (int i = 0; i < img.getWidth(); i++) {
+    public static ArrayList<Spike> GetSpike(BufferedImage img) {
+        ArrayList<Spike> list = new ArrayList<>();
+        for(int j = 0; j < img.getHeight(); j++)
+            for(int i = 0; i < img.getWidth(); i++) {
                 Color color = new Color(img.getRGB(i, j));
                 int value = color.getBlue();
                 if (value == SPIKE)
                     list.add(new Spike(i * Game.TILES_SIZE, j * Game.TILES_SIZE, SPIKE));
             }
+        return list;
+    }
 
+    public static ArrayList<WaterTop> GetWaterTop(BufferedImage img) {
+        ArrayList<WaterTop> list = new ArrayList<>();
+        for(int j = 0; j < img.getHeight(); j++)
+            for(int i = 0; i < img.getWidth(); i++) {
+                Color color = new Color(img.getRGB(i, j));
+                int value = color.getBlue();
+                if (value == WATER_TOP)
+                    list.add(new WaterTop(i * Game.TILES_SIZE, j * Game.TILES_SIZE, WATER_TOP));
+            }
+        return list;
+    }
+
+    public static ArrayList<WaterBottom> GetWaterBottom(BufferedImage img) {
+        ArrayList<WaterBottom> list = new ArrayList<>();
+        for(int j = 0; j < img.getHeight(); j++)
+            for(int i = 0; i < img.getWidth(); i++) {
+                Color color = new Color(img.getRGB(i, j));
+                int value = color.getBlue();
+                if (value == WATER_BOTTOM)
+                    list.add(new WaterBottom(i * Game.TILES_SIZE, j * Game.TILES_SIZE, WATER_BOTTOM));
+            }
         return list;
     }
 
     public static ArrayList<Cannon> GetCannons(BufferedImage img) {
         ArrayList<Cannon> list = new ArrayList<>();
-
-        for (int j = 0; j < img.getHeight(); j++)
-            for (int i = 0; i < img.getWidth(); i++) {
+        for(int j = 0; j < img.getHeight(); j++)
+            for(int i = 0; i < img.getWidth(); i++) {
                 Color color = new Color(img.getRGB(i, j));
                 int value = color.getBlue();
                 if (value == CANNON_LEFT || value == CANNON_RIGHT)
                     list.add(new Cannon(i * Game.TILES_SIZE, j * Game.TILES_SIZE, value));
             }
-
         return list;
     }
-
 }
--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -1,5 +1,6 @@
 package utilz;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -7,40 +8,50 @@ import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-import javax.imageio.ImageIO;
-
 public class LoadSave {
 
-    public static final String PLAYER_ATLAS = "player_sprites.png";
-    public static final String LEVEL_ATLAS = "outside_sprites.png";
-    public static final String MENU_BUTTONS = "button_atlas.png";
-    public static final String MENU_BACKGROUND = "menu_background.png";
-    public static final String PAUSE_BACKGROUND = "pause_menu.png";
-    public static final String SOUND_BUTTONS = "sound_button.png";
-    public static final String URM_BUTTONS = "urm_buttons.png";
-    public static final String VOLUME_BUTTONS = "volume_buttons.png";
-    public static final String MENU_BACKGROUND_IMG = "background_menu.png";
-    public static final String PLAYING_BG_IMG = "playing_bg_img.png";
-    public static final String BIG_CLOUDS = "big_clouds.png";
-    public static final String SMALL_CLOUDS = "small_clouds.png";
-    public static final String CRABBY_SPRITE = "crabby_sprite.png";
-    public static final String STATUS_BAR = "health_power_bar.png";
-    public static final String COMPLETED_IMG = "completed_sprite.png";
-    public static final String POTION_ATLAS = "potions_sprites.png";
-    public static final String CONTAINER_ATLAS = "objects_sprites.png";
-    public static final String TRAP_ATLAS = "trap_atlas.png";
-    public static final String CANNON_ATLAS = "cannon_atlas.png";
-    public static final String CANNON_BALL = "ball.png";
-    public static final String DEATH_SCREEN = "death_screen.png";
-    public static final String OPTIONS_MENU = "options_background.png";
+    public static final String PLAYER_ATLAS = "res/player_sprites.png";
+    public static final String LEVEL_ATLAS = "res/outside_sprites.png";
+    public static final String MENU_BUTTONS =  "res/button_atlas.png";
+    public static final String MENU_BACKGROUND =  "res/menu_background.png";
+    public static final String PAUSE_BACKGROUND =  "res/pause_menu.png";
+    public static final String SOUND_BUTTON =  "res/sound_button.png";
+    public static final String URM_BUTTONS =  "res/urm_buttons.png";
+    public static final String VOLUME_BUTTONS =  "res/volume_buttons.png";
+    public static final String MENU_BACKGROUND_IMG =  "res/background_menu.png";
+    public static final String PLAYING_BG_IMG =  "res/playing_bg_img.png";
+    public static final String BIG_CLOUDS =  "res/big_clouds.png";
+    public static final String SMALL_CLOUDS =  "res/small_clouds.png";
+    public static final String CRABBY_SPRITE =  "res/crabby_sprite.png";
+    public static final String STATUS_BAR = "res/health_power_bar.png";
+    public static final String COMPLETED_IMG = "res/completed_sprite.png";
+    public static final String WATER_BOTTOM = "res/water.png";
+    public static final String WATER_TOP = "res/water_atlas_animation.png";
+    public static final String MAGIC_BEAN = "res/magic_bean_img.png";
+
+    public static final String POTION_ATLAS = "res/potions_sprites.png";
+    public static final String CONTAINER_ATLAS = "res/objects_sprites.png";
+    public static final String TRAP_ATLAS = "res/trap_atlas.png";
+    public static final String CANNON_ATLAS = "res/cannon_atlas.png";
+    public static final String CANNON_BALL = "res/ball.png";
+    public static final String DEATH_SCREEN = "res/death_screen.png";
+    public static final String OPTIONS_MENU = "res/options_background.png";
+    public static final String BOOMERANG_ATLAS = "res/boomerang_atlas.png"; //extra
+
+    //    public static final String LEVEL_MENU_BACKGROUND = "res/gunny.jpg";
+    public static final String LEVEL_FRAMES = "res/level_frames.png";
+    public static final String RANKING_FRAME = "res/ranking_frame.png";
+
 
     public static BufferedImage GetSpriteAtlas(String fileName) {
         BufferedImage img = null;
         InputStream is = LoadSave.class.getResourceAsStream("/" + fileName);
+
         try {
             img = ImageIO.read(is);
 
         } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         } finally {
             try {
@@ -53,7 +64,7 @@ public class LoadSave {
     }
 
     public static BufferedImage[] GetAllLevels() {
-        URL url = LoadSave.class.getResource("/lvls");
+        URL url = LoadSave.class.getResource("/res/lvls");
         File file = null;
 
         try {
@@ -65,23 +76,26 @@ public class LoadSave {
         File[] files = file.listFiles();
         File[] filesSorted = new File[files.length];
 
-        for (int i = 0; i < filesSorted.length; i++)
-            for (int j = 0; j < files.length; j++) {
-                if (files[j].getName().equals((i + 1) + ".png"))
+        for(int i = 0; i < filesSorted.length; i++)
+            for(int j = 0; j < files.length; j++) {
+                if(files[j].getName().equals((i + 1) + ".png"))
                     filesSorted[i] = files[j];
 
             }
 
         BufferedImage[] imgs = new BufferedImage[filesSorted.length];
 
-        for (int i = 0; i < imgs.length; i++)
+        for(int i = 0; i < imgs.length; i++) {
             try {
                 imgs[i] = ImageIO.read(filesSorted[i]);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
 
         return imgs;
     }
+
+
 
 }
