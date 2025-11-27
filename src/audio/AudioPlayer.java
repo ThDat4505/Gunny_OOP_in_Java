2ba0efb33bdda16fcfd14ab528d11587bfcca6dd
ThDat4505 <datnguyen0571@gmail.com>
2025-11-27 13:47:08 +0700

    update AudioPlayer class

--- src/audio/AudioPlayer.java
+++ src/audio/AudioPlayer.java
@@ -1,19 +1,11 @@
 package audio;
 
+import javax.sound.sampled.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Random;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.BooleanControl;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
 public class AudioPlayer {
-
     public static int MENU_1 = 0;
     public static int LEVEL_1 = 1;
     public static int LEVEL_2 = 2;
@@ -28,7 +20,7 @@ public class AudioPlayer {
 
     private Clip[] songs, effects;
     private int currentSongId;
-    private float volume = 0.5f;
+    private float volume = 1f;
     private boolean songMute, effectMute;
     private Random rand = new Random();
 
@@ -39,10 +31,11 @@ public class AudioPlayer {
     }
 
     private void loadSongs() {
-        String[] names = { "menu", "level1", "level2" };
+        String[] names = {"menu", "level1", "level2"};
         songs = new Clip[names.length];
-        for (int i = 0; i < songs.length; i++)
+        for(int i = 0; i < songs.length; i++)
             songs[i] = getClip(names[i]);
+
     }
 
     private void loadEffects() {
@@ -51,42 +44,39 @@ public class AudioPlayer {
         for (int i = 0; i < effects.length; i++)
             effects[i] = getClip(effectNames[i]);
 
-        updateEffectsVolume();
-
+        updateEffecsVolume();
     }
 
     private Clip getClip(String name) {
-        URL url = getClass().getResource("/audio/" + name + ".wav");
+        URL url = getClass().getResource("/res/audio/" + name + ".wav");
         AudioInputStream audio;
 
         try {
             audio = AudioSystem.getAudioInputStream(url);
             Clip c = AudioSystem.getClip();
             c.open(audio);
+
             return c;
-
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-
             e.printStackTrace();
         }
 
         return null;
-
     }
 
     public void setVolume(float volume) {
         this.volume = volume;
         updateSongVolume();
-        updateEffectsVolume();
+        updateEffecsVolume();
     }
 
     public void stopSong() {
-        if (songs[currentSongId].isActive())
+        if(songs[currentSongId].isActive())
             songs[currentSongId].stop();
     }
 
     public void setLevelSong(int lvlIndex) {
-        if (lvlIndex % 2 == 0)
+        if(lvlIndex % 2 == 0)
             playSong(LEVEL_1);
         else
             playSong(LEVEL_2);
@@ -119,19 +109,19 @@ public class AudioPlayer {
 
     public void toggleSongMute() {
         this.songMute = !songMute;
-        for (Clip c : songs) {
+        for(Clip c : songs) {
             BooleanControl booleanControl = (BooleanControl) c.getControl(BooleanControl.Type.MUTE);
             booleanControl.setValue(songMute);
         }
     }
 
-    public void toggleEffectMute() {
+    public void toggleEffectsMute() {
         this.effectMute = !effectMute;
         for (Clip c : effects) {
             BooleanControl booleanControl = (BooleanControl) c.getControl(BooleanControl.Type.MUTE);
             booleanControl.setValue(effectMute);
         }
-        if (!effectMute)
+        if(!effectMute)
             playEffect(JUMP);
     }
 
@@ -141,10 +131,9 @@ public class AudioPlayer {
         float range = gainControl.getMaximum() - gainControl.getMinimum();
         float gain = (range * volume) + gainControl.getMinimum();
         gainControl.setValue(gain);
-
     }
 
-    private void updateEffectsVolume() {
+    private void updateEffecsVolume() {
         for (Clip c : effects) {
             FloatControl gainControl = (FloatControl) c.getControl(FloatControl.Type.MASTER_GAIN);
             float range = gainControl.getMaximum() - gainControl.getMinimum();
@@ -152,6 +141,4 @@ public class AudioPlayer {
             gainControl.setValue(gain);
         }
     }
-
 }
-
